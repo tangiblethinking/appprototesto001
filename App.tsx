67a1a0a5043029591050341b39dd8c5da0cb49d0
tangiblethinking <80935836+tangiblethinking@users.noreply.github.com>
2026-04-24 10:01:56 -0700

    Fix versioned CDN imports: sonner@2.0.3 → sonner, html2pdf.js@0.10.1 → html2pdf.js

--- App.tsx
+++ App.tsx
@@ -11,12 +11,12 @@ import { Textarea_4 } from "./components/textarea_4";
 import { Textarea_5 } from "./components/textarea_5";
 import { Textarea_6 } from "./components/textarea_6";
 import { Plus, Minus, Pencil, ChevronLeft, ChevronRight, Save, Share2, GripVertical, RotateCcw, FileText, Code } from "lucide-react";
-import { toast } from "sonner@2.0.3";
+import { toast } from "sonner";
 import { Toaster } from "./components/ui/sonner";
 import pako from "pako";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import * as html2pdfLib from "html2pdf.js@0.10.1";
+import * as html2pdfLib from "html2pdf.js";
 import { FABTooltip } from "./components/FABTooltip";
 
 interface TableData {
